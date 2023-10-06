a1877e073e33d9306a2d3e01ac015243ccad7ee7
Raúl García <rulu158@gmail.com>
2023-10-06 11:17:06 +0200

    Version 1.0.0

--- src/test/java/dev/bracers/rickandmorty/RickandmortyApplicationTests.java
+++ src/test/java/dev/bracers/rickandmorty/RickandmortyApplicationTests.java
@@ -1,13 +1,7 @@
 package dev.bracers.rickandmorty;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RickandmortyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
